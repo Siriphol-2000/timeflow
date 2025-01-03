9594ea8b5ddd2c8e4b13f58c92ccb32c6b2e2c2a
jiranuwat <jiranuwat12@gmail.com>
2025-01-03 09:53:53 +0700

    delete file

--- src/routes/testexport.rs
+++
@@ -1,7 +0,0 @@
-use actix_web::web;
-
-use crate::handler::testexport::timsheet_export;
-
-pub fn config_export(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/api/v1/timesheet").route("/export", web::post().to(timsheet_export)));
-}
